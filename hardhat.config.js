3e570e34be8b4c9fd2ff957345a1ae9eaaec3b80
Giovanni Cruz <giovannicruzvinhedo@gmail.com>
2025-09-04 23:21:51 -0300

    feat: enhance hardhat project with deployment and verification scripts
    - Updated package.json to include hardhat scripts for cleaning, compiling, testing, deploying, and verifying contracts.
    - Added dotenv for environment variable management.
    - Included new dependencies: @nomicfoundation/hardhat-verify and @nomiclabs/hardhat-etherscan.
    - Created a deploy script that deploys the SimpleStorage contract and verifies it on Etherscan if applicable.
    - Added a .env.example file for environment variable setup.
    - Implemented a block-number task to fetch and display the current block number.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,32 @@
 require("@nomiclabs/hardhat-waffle")
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      // accounts: "Thanks Hardhat, you provide them!",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
   solidity: "0.8.4",
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,45 @@
 const hre = require("hardhat")
+const { run, network } = require("hardhat")
 
 async function main() {
   const SimpleStorageFactory = await hre.ethers.getContractFactory(
     "SimpleStorage"
   )
   console.log("Deploying contract...")
-
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.deployed()
-  console.log("MyContract deployed to:", simpleStorage.address)
+  console.log("Contract deployed to: ", simpleStorage.address)
+
+  // What happens when we deploy to a network other than hardhat network?
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block txes...")
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current Value is: ${currentValue}`)
+
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(12)
+  await transactionResponse.wait(1)
+  console.log("Updated value:", await simpleStorage.retrieve())
+}
+
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!")
+    } else {
+      console.log(error)
+    }
+  }
 }
 
 main()
